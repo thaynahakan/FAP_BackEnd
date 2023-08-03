5097b154511a6b0c0206b6126266118ca5d607f0
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-08-03 19:00:58 -0300

    inclusão feita do try e catch no while

--- modulo2/ex007.js
+++ modulo2/ex007.js
@@ -1,16 +1,26 @@
 /* Desenvolva um programa que recebe do usuário nome completo e ano de nascimento que seja entre 1922 e 2021. A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, no ano atual (2022).
 
-Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.
-*/
+Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.*/
 
 let nome = prompt("Digite o seu nome completo: ");
 let ano_nascimento;
 
-do {
-  ano_nascimento = prompt("Digite o seu ano de nascimento:");
-  ano_nascimento = parseFloat(ano_nascimento);
-} while (isNaN(ano_nascimento) || ano_nascimento < 1922 || ano_nascimento > 2021);
+while (true) {
+  try {
+    ano_nascimento = prompt("Digite o seu ano de nascimento:");
+    ano_nascimento = parseFloat(ano_nascimento);
+    
+    if (isNaN(ano_nascimento) || ano_nascimento < 1922 || ano_nascimento > 2021) {
+       throw new Error("Ano inválido, tente novamente.");
+    }
+
+  
+    break;
+  } catch (error) {
+     alert(error.message);
+  }
+}
 
 let atual = 2022 - ano_nascimento;
 
-console.log(nome + ", esse ano você completa " + atual + " anos.");
+console.log(nome + ", em 2022 você completa " + atual + " anos.");
