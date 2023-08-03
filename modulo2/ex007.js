09e6f05faf3df935b9c5c83079911689ff707747
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-08-03 14:51:27 -0300

    atualização da atividade

--- modulo2/ex007.js
+++ modulo2/ex007.js
@@ -1,10 +1,13 @@
-const readline = require("readline");
+/* Desenvolva um programa que recebe do usuário nome completo e ano de nascimento que seja entre 1922 e 2021. A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, no ano atual (2022).
 
-let nome = readline("Digite o seu nome completo: ");
+Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.
+*/
+
+let nome = prompt("Digite o seu nome completo: ");
 let ano_nascimento;
 
 do {
-  ano_nascimento = readline("Digite o seu ano de nascimento:");
+  ano_nascimento = prompt("Digite o seu ano de nascimento:");
   ano_nascimento = parseFloat(ano_nascimento);
 } while (isNaN(ano_nascimento) || ano_nascimento < 1922 || ano_nascimento > 2021);
 
