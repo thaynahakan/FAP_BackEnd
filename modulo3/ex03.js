a6827a8cb1e9a851edf4aef2654f758709231ae5
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-08-08 13:48:56 -0300

    atualização

---
+++ modulo3/ex03.js
@@ -0,0 +1,19 @@
+let num = [3, 7, 9, 1, 0]
+// Um esboço que a questão pede seria:
+// Usando Filas o primeiro elemento a ser inserido é o primeiro a ser removido.
+function removerFila(num) { 
+  return num.shift();
+}// Fila sairia por ordem : 3, 7, 9, 1, 0
+
+// Usando Pilhas o último elemento inserido é o primeiro a ser removido.
+function removerPilha(num) {
+  return num.pop();
+} // Pilha sairia por ordem: 0, 1, 9, 7, 3
+
+// Usando Listas não tem uma ordem fixa de remoção como uma fila ou uma pilha.
+function removerLista(num) {
+  return lista.pop();
+} // Lista por ordem: 0, 1, 9, 7, 3
+// a função pop é usada para retirar o ultimo elemento da lista.
+
+// Não finalizei o código de propósito pois era mais pra fins demonstrativos das funções.
