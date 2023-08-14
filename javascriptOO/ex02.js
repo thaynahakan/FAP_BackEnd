6ddb4b8bc3e7044f77cf1c70dc737493d2dde52c
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-08-14 14:20:11 -0300

    pessoinha linda

---
+++ javascriptOO/ex02.js
@@ -0,0 +1,13 @@
+//Vamos criar um cadastro de pessoa com os seguintes dados: nome, salário, idade e se possui diploma. Antes de começar a codificação, identifiquem os tipos de cada variável. Em seguida, desenvolvam o código utilizando exemplos para cada variável que respeitem os tipos identificados. Certifiquem-se de que os valores correspondam aos tipos de dados corretos.
+
+const pessoinha = {
+    nome : "Pipitinha Cheirosah",
+    idade : 54,
+    salario : 33,
+    diploma : false
+};
+
+console.log(`O nome da pessoinha é ${pessoinha.nome}`)
+console.log(`A idade da pessoinha é ${pessoinha.idade}`)
+console.log(`A pessoinha é pobrinha ${pessoinha.salario}`)
+console.log(`A pobre da pessoinha tem diploma?????? ${pessoinha.diploma}`)
