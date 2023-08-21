046079115c244b4d8c6d4bdf652114ff1bec151a
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-08-21 10:37:51 -0300

    calculadora bonitinha

---
+++ javascriptOO/ex04.js
@@ -0,0 +1,38 @@
+/* Instruções JavaScript 
+Com os conceitos aprendidos, crie um programa de calculadora que
+receba dois valores, que devem ser salvos em variáveis;
+o usuário deve colocar qual operador ele vai utilizar por meio dos simbolos aritméticos;
+com os dois valores e o operador definido, o programa deve fazer a operação e retornar o resultado;
+se houver divisão, você deve retornar o resultado e a sobra, caso haja alguma. */
+
+let n1 = parseInt(prompt("Digite o primeiro valor: "));
+let n2 = parseInt(prompt("Digite o segundo valor: "));
+
+let operador = prompt("Digite o operador desejado (*, +, -,/): ");
+
+let sobra;
+let resultado;
+
+switch (operador){
+  case "+":
+    resultado = n1 + n2;
+    break;
+  case "-":
+    resultado = n1 - n2;
+    break;
+  case "*":
+    resultado = n1 * n2;
+    break;
+  case "/":
+    resultado = n1 / n2;
+    sobra = n1 % n2;
+    break;
+  default:
+    console.log("Operador inválido");
+}
+
+if (operador === "/" && sobra !== undefined) {
+  console.log(`Resultado: ${resultado.toFixed(2)}, Sobra: ${sobra} `);
+} else if (resultado !== undefined) {
+  console.log(`Resultado: ${resultado.toFixed(2)}`)
+}
