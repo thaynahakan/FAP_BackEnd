3bafcfc4f4cf1f3b8cb10a44e3dd41abc26add5b
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-08-21 14:09:23 -0300

    expliquei, sim, mas, nao gostei

--- javascriptOO/ex.06.js
+++ javascriptOO/ex.06.js
@@ -6,19 +6,23 @@ Uma arrow function com parâmetros e que retorne um valor.
 
 Crie um programa que utilize essas três funções de forma lógica, por exemplo: um programa de calculadora. */
 
+// função tradicional com a palavra reservada "Function" e sem nenhum parâmetro
 function mensagem() {
     console.log("CaraMaum que morde a onda vela.");
 };
 
+// Função tradicional com parâmetros e retorno de valor
 function somacao(a,b) {
     return a + b;
 }
 
+// Uma arrow function com parâmetros e que retorne um valor.
 const diminuicao = (a,b) => a - b ;
 
 let a = 6;
 let b = 6;
 
-mensagem()
-console.log(somacao(a,b))
+// Desempacotando
+mensagem() // Já retorna um print
+console.log(somacao(a,b)) // precisa do console.log pra desempacotar.
 console.log(diminuicao(a,b))
