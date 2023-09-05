1e92cfa42f72ebd4dba10f3953664f3d72959623
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-09-05 17:27:19 -0300

    verificar par ou impar com uma pergunta

---
+++ javascriptOO/ex11.js
@@ -0,0 +1,22 @@
+const readline = require('readline-sync');
+
+function verificar(numero) {
+    if (numero % 2 === 0) {
+        return 'Par';
+    } else {
+        return 'Ímpar';
+    }
+}
+
+
+while (true) {
+    const input = readline.question('Digite o número: ');
+    const numero = parseInt(input);
+
+    if (!isNaN(numero)) {
+        console.log(`O número que você digitou ${numero} é um número ${verificar(numero)}!`);
+        break;
+    } else {
+        console.log('Entrada inválida. Por favor, digite um número inteiro.');
+    }
+}
