7cce849c897fd86d875d1e23ee68e4f81670285a
Thayná Bittencourt Baima <saidai_thayna@hotmail.com>
2023-09-05 16:47:19 -0300

    att

--- javascriptOO/ex10.js
+++ javascriptOO/ex10.js
@@ -1,15 +1,10 @@
 // Usando a linguagem Javascript, crie um código que tenha uma classe e um método estático
 
 class Pe {
-    constructor(meia, anel){
-        this.meia = "anel";
-        this.anel = "meia";
-    };
-    _coisasdePes(){
-        console.log(`Este delicioso pé, usa ${this.meia} e um ${this.anel} no dedo mindinho. `)
-    };
+     static coisasdePes(){
+        return 'Este delicioso pé, usa um anel  e uma meia no dedo mindinho.'
+};
 };
 
 
-const novoPe = new Pe("meinha fidida","quetinho")
-novoPe._coisasdePes()
+console.log(Pe.coisasdePes());
