f7f591fc56e24aabcaa987ca0e3db71ec8922c86
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2023-12-12 15:45:12 -0300

    const do prompt

--- javascriptOO/ex.05.js
+++ javascriptOO/ex.05.js
@@ -1,5 +1,7 @@
 /* Crie dois códigos de sistema de notas para uma escola. O primeiro código deve ser um programa que informa se o aluno reprovou, ou não, com base nas três notas que ele adicionou ao programa. Utilize, no mínimo, um operador de atribuição e um operador ternário. O segundo código é um programa que o aluno deve escrever duas notas e o retorno informa a nota mínima que ele deve tirar na próxima prova para poder passar com nota sete.*/
 
+const prompt = require(`prompt-sync`)();
+
 let nota1 = parseFloat(prompt("Digite sua primeira nota: "));
 let nota2 = parseFloat(prompt("Digite sua segunda nota: "));
 let nota3 = parseFloat(prompt("Digite sua terceira nota: "));
