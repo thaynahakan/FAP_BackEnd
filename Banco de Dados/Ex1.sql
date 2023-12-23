7285506fd5ae636a828b806e806ded4858e2cc77
Thayna Bittencourt Baima <131307382+thaynahakan@users.noreply.github.com>
2023-12-23 14:26:01 -0300

    att

--- Banco de Dados/Ex1.sql
+++ Banco de Dados/Ex1.sql
@@ -1,36 +1,11 @@
-//Inserção de dados
-INSERT INTO Autores (Nome, Nacionalidade) VALUES ('Marcos Aurélio', 'Brasileiro');
-INSERT INTO Autores (Nome, Nacionalidade) VALUES ('José Vinícius', 'Espanhol');
-INSERT INTO Autores (Nome, Nacionalidade) VALUES ('Matt Vine', 'Francês');
+CREATE DATABASE exercicio_db; USE exercicio_db; CREATE TABLE IF NOT EXISTS db_livro ( id_livro INT AUTO_INCREMENT PRIMARY KEY, autor_livro VARCHAR(50) NOT NULL, nome_livro VARCHAR(100) NOT NULL, publi_livro DATE NOT NULL );
 
-INSERT INTO Livros (Titulo, AnoPublicacao, AutorID) VALUES ('A Vida', 2015, 1);
-INSERT INTO Livros (Titulo, AnoPublicacao, AutorID) VALUES ('Caminos Perdidos', 2013, 2);
-INSERT INTO Livros (Titulo, AnoPublicacao, AutorID) VALUES ('A Terra', 2017, 1);
-INSERT INTO Livros (Titulo, AnoPublicacao, AutorID) VALUES ('Ce La Vie', 2021, 3);
-INSERT INTO Livros (Titulo, AnoPublicacao, AutorID) VALUES ('El Tiempo', 2015, 2);
+SHOW TABLES;
 
-//Simples:
-SELECT * FROM Autores;
-SELECT * FROM Livros;
+CREATE TABLE IF NOT EXISTS db_autor ( id_autor INT AUTO_INCREMENT PRIMARY KEY, nome_autor VARCHAR(50) NOT NULL, pais_autor VARCHAR(100) NOT NULL, nascim_autor DATE NOT NULL );
 
-//INNER JOIN:
-SELECT Autores.Nome AS Autor, Livros.Titulo
-FROM Autores
-INNER JOIN Livros ON Autores.AutorID = Livros.AutorID;
+CREATE TABLE IF NOT EXISTS db_usuario ( id_usuario INT AUTO_INCREMENT PRIMARY KEY, nome_usuario VARCHAR(50) NOT NULL, cpf_usuario INT NOT NULL, tel_usuario INT NOT NULL );
 
-//LEFT JOIN:
-SELECT Autores.Nome AS Autor, Livros.Titulo
-FROM Autores
-LEFT JOIN Livros ON Autores.AutorID = Livros.AutorID;
+CREATE TABLE IF NOT EXISTS db_emprestimo ( id_emprestimo INT AUTO_INCREMENT PRIMARY KEY, quant_livro INT NOT NULL, id_livro INT NOT NULL, data_emprestimo DATE NOT NULL );
 
-//Filtro por nacionalidade:
-SELECT Autores.Nome AS Autor, Livros.Titulo
-FROM Autores
-LEFT JOIN Livros ON Autores.AutorID = Livros.AutorID
-WHERE Autores.Nacionalidade = 'Espanhol';
-
-//Consulta agregada:
-SELECT Autores.Nome AS Autor, COUNT(Livros.LivroID) AS QuantidadeLivros
-FROM Autores
-LEFT JOIN Livros ON Autores.AutorID = Livros.AutorID
-GROUP BY Autores.Nome;
+CREATE TABLE IF NOT EXISTS db_devolucao ( id_devolucao INT AUTO_INCREMENT PRIMARY KEY, data_devolucao DATE NOT NULL, id_livro INT NOT NULL, id_usuario INT NOT NULL );
